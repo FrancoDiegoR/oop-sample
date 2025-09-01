c1639f623895a6f679f8c62b962e64228949533f
Franco Rioja <francodiegorioja@gmail.com>
2025-09-01 11:49:46 -0500

    fix: add missing assigment

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ACME.OOP.Procurement.Domain.Model.Aggregate;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.Aggregate;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.ValueObjets;
 
-Console.WriteLine("Hello, World!");
+var supplierAddress = new Address("Supplier Street", "123", "Supplier City", 
+    null, "12345", "United States");
+
+var supplier = new Supplier("SUP001", "Microsoft, Inc", supplierAddress);
+    
+var purcharseOrder = new PurcharseOrder("P0001", new SupplierId(supplier.Identifier), 
+    DateTime.Now, "USD");
+        
+purcharseOrder.AddItem(ProductId.New(), 10, 25.99m)
+    .AddItem(ProductId.New(), 10, 25.99m);
+            
+Console.WriteLine($"Purchase order : {purcharseOrder.OrderNumber} created by {supplier.Identifier}");
+
+foreach (var item in purcharseOrder.Items)
+{
+    Console.WriteLine($"Order item Total: {item.CalculateItemTotal()}");
+}
+            
+Console.WriteLine($"Total Order Amount: {purcharseOrder.CalculateOrderTotal()}");
